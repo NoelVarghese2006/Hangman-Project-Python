fa7d3c7bd4b3ff90c7d4b2fbe7d37f8a45a3b50c
ThetrueDrleoN <noelvarghese2006@gmail.com>
2024-09-20 23:59:31 -0500

    Initial Commit

---
+++ Hangman.py
@@ -0,0 +1,54 @@
+import random
+
+with open("quotes.txt", "r") as file:
+    lines = file.readlines()
+    randLine = random.choice(lines)
+    randLine = randLine[:-1]
+    randLine = randLine.lower()
+
+print("How many lives do you want?")
+validInput = False
+while(not validInput):
+    userInput = input("Easy - 9, Normal - 6, Hard - 3 \n")
+    lives = int(userInput)
+    if(lives in [3, 6, 9]):
+        validInput = True
+
+blanks = ""
+print("Hangman Game")
+print("Guess the quote")
+for  i in randLine:
+    if(i == " "):
+        blanks += " "
+    elif(not i.isalpha()):
+        blanks += i
+    else:
+        blanks += "_"
+gameLoop = True
+
+while(gameLoop):
+    print()
+    print("Lives: " + str(lives))
+    print(blanks)
+    print()
+
+    validate = False
+    while(not validate):
+        userInput = input("Enter a letter: \n")
+        if(userInput.isalpha() and len(userInput) == 1):
+            validate = True
+
+    indices = [i for i, c in enumerate(randLine) if c == userInput]
+    for i in indices:
+        blanks = blanks[:i] + userInput + blanks[i + 1:]
+    if(len(indices) == 0):
+        lives = lives - 1
+    if(lives == 0):
+        print("Sorry, you lost")
+        print(randLine)
+        gameLoop = False
+    if(blanks.find("_") == -1):
+        print("You win")
+        print(randLine)
+        gameLoop = False
+    
